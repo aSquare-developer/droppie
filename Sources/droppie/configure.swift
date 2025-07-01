8c5d479babec3da65edd8aac3ad4f21ed3280468
aSquare <artur.anissimov@gmail.com>
2025-07-01 13:17:24 +0300

    Database configurations

--- Sources/droppie/configure.swift
+++ Sources/droppie/configure.swift
@@ -8,30 +8,43 @@ public func configure(_ app: Application) async throws {
     
 //    app.http.server.configuration.hostname = "0.0.0.0"
     
-    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-        postgresConfig.tlsConfiguration = .makeClientConfiguration()
-        postgresConfig.tlsConfiguration?.certificateVerification = .none
-        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-    } else {
-        app.databases.use(
-            .postgres(
-                configuration: .init(
-                    hostname: Environment.get("DB_HOST_NAME") ?? "",
-                    username: Environment.get("DB_USER_NAME") ?? "",
-                    password: Environment.get("DB_PASSWORD") ?? "",
-                    database: Environment.get("DB_NAME") ?? "",
+    if let databaseURL = Environment.get("DATABASE_URL") {
+            do {
+                var tls = TLSConfiguration.makeClientConfiguration()
+                tls.certificateVerification = .none
+                
+                var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+                postgresConfig = SQLPostgresConfiguration(
+                    hostname: postgresConfig.coreConfiguration.host ?? "",
+                    port: postgresConfig.coreConfiguration.port ?? 5432,
+                    username: postgresConfig.coreConfiguration.username,
+                    password: postgresConfig.coreConfiguration.password,
+                    database: postgresConfig.coreConfiguration.database,
                     tls: .disable
                 )
-            ),
-            as: .psql
-        )
-    }
+
+                app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+            } catch {
+                app.logger.report(error: error)
+                fatalError("Invalid DATABASE_URL: \(error)")
+            }
+        } else {
+            let config = SQLPostgresConfiguration(
+                hostname: Environment.get("DB_HOST_NAME") ?? "",
+                port: 5432,
+                username: Environment.get("DB_USER_NAME") ?? "",
+                password: Environment.get("DB_PASSWORD") ?? "",
+                database: Environment.get("DB_NAME") ?? "",
+                tls: .disable
+            )
+
+            app.databases.use(.postgres(configuration: config), as: .psql)
+        }
+
     
     // Register migrations
     app.migrations.add(CreateUsersTableMigration())
-//    app.migrations.add(CreateProfilesTableMigration())
     app.migrations.add(CreateRoutesTableMigration())
-//    app.migrations.add(CreateRoutesListTableMigration())
     
     // Register controllers
     try app.register(collection: UserController())
